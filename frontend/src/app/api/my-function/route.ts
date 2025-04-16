2f3d718df2e072beac9bf16122c16692f29720c5
RockySheoran <rockysheoran72@gmail.com>
2025-04-16 16:24:42 +0530

    change

--- frontend/src/app/api/my-function/route.ts
+++ frontend/src/app/api/my-function/route.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-export const maxDuration = 30 // This function can run for a maximum of 5 seconds
+export const maxDuration = 60 // This function can run for a maximum of 5 seconds
 
 export function GET(request: Request) {
   return new Response("Vercel", {
