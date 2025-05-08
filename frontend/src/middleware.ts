f3f4bf964d87d9a509fc5eaee34c4d715d8caeae
Rocky Sheoran <156376171+RockySheoran@users.noreply.github.com>
2025-05-08 22:00:41 +0530

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,6 +6,8 @@ import { JWT } from "next-auth/jwt";
 
 interface MiddlewareRequest extends NextRequest {}
 
+
+
 export async function middleware(req: MiddlewareRequest): Promise<NextResponse> {
   const token: JWT | null = await getToken({ req });
   const { pathname, origin } = req.nextUrl;
